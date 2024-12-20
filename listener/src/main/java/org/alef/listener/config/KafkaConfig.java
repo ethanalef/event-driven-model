67888846f05b77952a8046975875be5aff2db171
ethanalef <ethanalef@gmail.com>
2024-12-20 17:35:07 +0800

    Fix kafka config

--- listener/src/main/java/org/alef/listener/config/KafkaConfig.java
+++ listener/src/main/java/org/alef/listener/config/KafkaConfig.java
@@ -1,9 +1,19 @@
 package org.alef.listener.config;
 
+import org.alef.common.event.RemoteOrderEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -14,5 +24,19 @@ public class KafkaConfig {
                 .partitions(10)
                 .replicas(1)
                 .build();
+    }
+
+    @Bean
+    public ProducerFactory<String, RemoteOrderEvent> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, RemoteOrderEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
